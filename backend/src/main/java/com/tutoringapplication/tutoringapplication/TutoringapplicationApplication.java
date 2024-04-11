1dba9c8c42d59745d75a96b655297d08b56188aa
JGambino00 <66328776+JGambino00@users.noreply.github.com>
2024-04-11 13:22:57 -0400

    Added backend config

---
+++ backend/src/main/java/com/tutoringapplication/tutoringapplication/TutoringapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.tutoringapplication.tutoringapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoringapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoringapplicationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tutoringapplication/tutoringapplication/TutoringapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutoringapplication.tutoringapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoringapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
