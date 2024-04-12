7310e05fb361c79d0bc2186ec2026d03d4486bec
aaKugelblitz <smell.my.big.toe@me.com>
2024-04-12 15:21:50 -0400

    fixed username issue

--- backend/src/main/java/com/mentorme/dao/UserRepository.java
+++ backend/src/main/java/com/mentorme/dao/UserRepository.java
@@ -2,7 +2,9 @@ package com.mentorme.dao;
 
 import com.mentorme.model.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
 }
