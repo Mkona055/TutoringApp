03955d0dc4595e86f99c7f5b627f63c0bd7109ca
aaKugelblitz <smell.my.big.toe@me.com>
2024-04-18 17:06:46 -0400

    added postUpdate

--- backend/src/main/java/com/mentorme/controller/PostController.java
+++ backend/src/main/java/com/mentorme/controller/PostController.java
@@ -1,5 +1,8 @@
 package com.mentorme.controller;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.mentorme.dao.OfferRepository;
 import com.mentorme.dao.PostRepository;
@@ -14,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.session.HttpSessionDestroyedEvent;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -51,7 +55,7 @@ public class PostController {
     public ResponseEntity<Post> getPost(@PathVariable int id) {
         Post post = posts.findById(id).orElse(null);
 
-        if (post==null) {
+        if (post == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
             return new ResponseEntity<>(post, HttpStatus.OK);
@@ -96,9 +100,9 @@ public class PostController {
     ) {
         Set<Post> results = new HashSet<>(posts.findAll());
         // .retainAll() acts as intersection of two sets
-        if (tags!=null)             results.retainAll(posts.findPostsByTags_NameIn(tags));
-        if (maxHourlyRate!=null)    results.retainAll(posts.findPostsByHourlyRateIsLessThanEqual(maxHourlyRate));
-        if (location!=null)         results.retainAll(posts.findPostsByUser_Location(location));
+        if (tags != null)           results.retainAll(posts.findPostsByTags_NameIn(tags));
+        if (maxHourlyRate != null)  results.retainAll(posts.findPostsByHourlyRateIsLessThanEqual(maxHourlyRate));
+        if (location != null)       results.retainAll(posts.findPostsByUser_Location(location));
 
         return new ResponseEntity<>(new ArrayList<>(results), HttpStatus.OK);
     }
@@ -111,9 +115,9 @@ public class PostController {
     ) {
         Set<Request> results = new HashSet<>(requests.findAll());
         // .retainAll() acts as intersection of two sets
-        if (tags!=null)             results.retainAll(requests.findRequestsByTags_NameIn(tags));
-        if (maxHourlyRate!=null)    results.retainAll(requests.findRequestsByHourlyRateIsLessThanEqual(maxHourlyRate));
-        if (location!=null)         results.retainAll(requests.findRequestsByUser_Location(location));
+        if (tags != null)           results.retainAll(requests.findRequestsByTags_NameIn(tags));
+        if (maxHourlyRate != null)  results.retainAll(requests.findRequestsByHourlyRateIsLessThanEqual(maxHourlyRate));
+        if (location != null)       results.retainAll(requests.findRequestsByUser_Location(location));
 
         return new ResponseEntity<>(new ArrayList<>(results), HttpStatus.OK);
     }
@@ -126,9 +130,9 @@ public class PostController {
     ) {
         Set<Offer> results = new HashSet<>(offers.findAll());
         // .retainAll() acts as intersection of two sets
-        if (tags!=null)             results.retainAll(offers.findOffersByTags_NameIn(tags));
-        if (maxHourlyRate!=null)    results.retainAll(offers.findOffersByHourlyRateIsLessThanEqual(maxHourlyRate));
-        if (location!=null)         results.retainAll(offers.findOffersByUser_Location(location));
+        if (tags != null)           results.retainAll(offers.findOffersByTags_NameIn(tags));
+        if (maxHourlyRate != null)  results.retainAll(offers.findOffersByHourlyRateIsLessThanEqual(maxHourlyRate));
+        if (location != null)       results.retainAll(offers.findOffersByUser_Location(location));
 
         return new ResponseEntity<>(new ArrayList<>(results), HttpStatus.OK);
     }
@@ -156,12 +160,30 @@ public class PostController {
     }
 
     @PutMapping("/post/{id}/update")
-    public ResponseEntity<Post> updatePostById(@PathVariable int id, @RequestParam Post post) {
+    public ResponseEntity<Post> updatePostById(@PathVariable int id, @RequestBody Map<String,String> postFields) {
 
-        if (id != post.getId()) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (id != Integer.parseInt(postFields.get("id"))) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
-        posts.deleteById(id);
-        posts.save(post);
+        Post postToBeUpdated = posts.findById(id).orElse(null);
+        if (postToBeUpdated == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        //UPDATES TITLE, DESCRIPTION, HOURLY RATE, IN PERSON, TAGS (RESETS TAGS)
+        postToBeUpdated.setTitle(       postFields.get("title"));
+        postToBeUpdated.setDescription( postFields.get("description"));
+        postToBeUpdated.setHourlyRate(  Double.valueOf(postFields.get("hourlyRate")));
+        postToBeUpdated.setInPerson(    Boolean.getBoolean(postFields.get("inPerson")));
+
+        // Tags of String format "1,2,3" with relevant ids
+        postToBeUpdated.setTags(        new HashSet<>());
+        String[] tagStrings = postFields.get("tags").split(",");
+        for (String tagId : tagStrings) {
+            postToBeUpdated.addTag(
+                    tags.findTagById(Integer.parseInt(tagId))
+            );
+        }
+
+        posts.save(postToBeUpdated);
+
         return new ResponseEntity<>(posts.findPostById(id), HttpStatus.OK);
     }
 
@@ -178,38 +200,62 @@ public class PostController {
         }
     }
 
-    @PostMapping("/newpost")
-    public ResponseEntity<Post> createPost(@RequestBody Map<String,String> body) {
-        String type = body.get("role");
-
-        Post post;
-        if (type.equals("OFFER")) {
-            post = new Offer();
-        } else if (type.equals("REQUEST")) {
-            post = new Request();
-        } else {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        post.setDescription(
-                body.get("description"));
-        post.setId(
-                Integer.parseInt(body.get("id")));
-        post.setInPerson(
-                Boolean.getBoolean(body.get("inPerson")));
-
-        RestTemplate rt = new RestTemplate();
-        ResponseEntity<User> user = rt.getForEntity("http://localhost:8080/users/user/{id}", User.class, body.get("user_id"));
-        post.setUser( user.getBody() );
-
-        int[] tagIds =  new Gson().fromJson(body.get("tags"), int[].class);
-        for (int tagId : tagIds) {
-            post.addTag(tags.findTagById(tagId));
-        }
-
-        posts.save(post);
-
-        return new ResponseEntity<>(posts.findPostById(post.getId()), HttpStatus.OK);
-    }
+//    @Transactional
+//    @PostMapping("/newrequest")
+//    public ResponseEntity<Post> createRequest(@RequestBody Request request) {
+//        posts.save(request);
+//        return new ResponseEntity<>(posts.findPostById(request.getId()), HttpStatus.OK);
+//    }
+//
+//    @Transactional
+//    @PostMapping("/newpost")
+////    public ResponseEntity<Post> createPost(@RequestBody Map<String,String> body) {
+//    public ResponseEntity<Post> createPost(@RequestBody Map<String,String> body) {
+//        try {
+//            Post post;
+//            if (body.get("type").equals("OFFER")) {
+//                post = new ObjectMapper().readValue(body.toString(), Offer.class);
+//            } else if (body.get("type").equals("REQUEST")) {
+//                post = new ObjectMapper().readValue(body.toString(), Request.class);
+//            } else {
+//                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//            }
+//
+//            posts.save(post);
+//
+//            return new ResponseEntity<>(posts.findPostById(post.getId()), HttpStatus.OK);
+//
+//        } catch (JsonProcessingException e) {
+//            throw new RuntimeException(e.getMessage(),e.getCause());
+////            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//        String type = body.get("type");
+//
+//        Post post;
+//        if (type.equals("OFFER")) {
+//            post = new Offer();
+//        } else if (type.equals("REQUEST")) {
+//            post = new Request();
+//        } else {
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//
+//        post.setDescription(
+//                body.get("description"));
+//        post.setId(
+//                Integer.parseInt(body.get("id")));
+//        post.setInPerson(
+//                Boolean.getBoolean(body.get("inPerson")));
+//
+//        RestTemplate rt = new RestTemplate();
+//        ResponseEntity<User> user = rt.getForEntity("http://localhost:8080/users/user/{id}", User.class, body.get("user_id"));
+//        post.setUser( user.getBody() );
+//
+////        int[] tagIds =  new Gson().fromJson(body.get("tags"), int[].class);
+//        String[] tagIds = body.get("tags").split(",");
+//        for (String tagId : tagIds) {
+//            post.addTag(tags.findTagById(Integer.parseInt(tagId)));
+//        }
+//
 
 }
